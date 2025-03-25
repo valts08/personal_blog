3e0213bede2b6f62c226d44679f01405c3218205
valts08 <valts.pilans@gmail.com>
2025-03-25 22:02:54 +0200

    changing base back to match 'name' in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/personal_blog',
   plugins: [react(), tailwindcss()],
 })
