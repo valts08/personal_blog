7d933032341460b26c671040d477f215aa317c42
valts08 <valts.pilans@gmail.com>
2025-04-12 13:44:50 +0300

    switched base path to 'personal_blog', added logic for theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import { Routes, Route } from "react-router-dom";
+import { ThemeContext } from "./contexts/ThemeContext";
 import Navbar from './components/Navbar'
 import Home from './pages/home/Home';
 import Blog from './pages/blog/Blog';
 import About from './pages/about/About';
 import Newsletter from './pages/newsletter/Newsletter';
 import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
 
+  const [globalDarkTheme, setGlobalDarkTheme] = useState<boolean | null>(false)
+
+  const handleGlobalThemeToggle = () => {
+    setGlobalDarkTheme(prevValue => !prevValue)
+  }
+
   return (
     <div className="mt-5 mx-auto max-w-9/10 md:max-w-8/10 lg:max-w-6/10">
-      <Navbar />
-      <div className="px-5 mx-auto max-w-98/100 border-x border-b bg-neutral-100 border-neutral-200">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/blog' element={<Blog />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/newsletter' element={<Newsletter />} />
-        </Routes>
-        <Footer />
-      </div>
+      <ThemeContext.Provider value={globalDarkTheme}>
+        <Navbar toggleFunc={handleGlobalThemeToggle}/>
+        <div className="px-5 mx-auto max-w-98/100 border-x border-b bg-neutral-100 border-neutral-200">
+          <Routes>
+            <Route path='personal_blog/' element={<Home />} />
+            <Route path='personal_blog/blog' element={<Blog />} />
+            <Route path='personal_blog/about' element={<About />} />
+            <Route path='personal_blog/newsletter' element={<Newsletter />} />
+          </Routes>
+          <Footer />
+        </div>
+      </ThemeContext.Provider>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
 import AvatarImg from '../../src/assets/images/image-avatar.jpg'
 import MoonIcon from '../../src/assets/images/icon-moon.svg'
 import SunIcon from '../../src/assets/images/icon-sun.svg'
+import { ThemeContext } from '../contexts/ThemeContext';
+import { useContext } from 'react';
 import { NavLink, Link } from "react-router-dom";
-import { useState } from "react";
 
-const Navbar = () => {
+interface VoidFunction {
+    toggleFunc: () => void // look up how to make interfaces reusable
+}
 
-    const [lightTheme, setLightTheme] = useState(true);
+const Navbar = ({ toggleFunc }: VoidFunction) => {
+
+    const globalDarkTheme = useContext(ThemeContext) // fix this later, don't leave any type
 
     const activeLinkStyles = "underline decoration-3 decoration-sky-500/50"
     const hoverLinkStyles = "hover:underline hover:decoration-3 hover:decoration-sky-500/50"
@@ -17,23 +22,23 @@ const Navbar = () => {
                 <img className="mx-auto border-1 rounded-xl border-neutral-200" src={AvatarImg} alt="profile-image" />
             </Link>
             <div className="flex gap-x-4 items-center">
-                <NavLink to="/" className={({ isActive }) => {
+                <NavLink to="personal_blog/" className={({ isActive }) => {
                     return isActive ? activeLinkStyles : hoverLinkStyles
                 }}> Home</NavLink>
-                <NavLink to="/blog" className={({ isActive }) => {
+                <NavLink to="personal_blog/blog" className={({ isActive }) => {
                     return isActive ? activeLinkStyles : hoverLinkStyles
                 }}> Blog</NavLink>
-                <NavLink to="/about" className={({ isActive }) => {
+                <NavLink to="personal_blog/about" className={({ isActive }) => {
                     return isActive ? activeLinkStyles : hoverLinkStyles
                 }}> About</NavLink>
-                <NavLink to="/newsletter" className={({ isActive }) => {
+                <NavLink to="personal_blog/newsletter" className={({ isActive }) => {
                     return isActive ? activeLinkStyles : hoverLinkStyles
                 }}> Newsletter</NavLink>
                 <button
                     className="w-[40px] h-[40px] border-1 rounded-lg border-neutral-200 bg-neutral-100 py-[5px]"
-                    onClick={() => setLightTheme(!lightTheme)}>
-                    <img className="mx-auto" src={lightTheme ? MoonIcon : SunIcon} 
-                    alt={lightTheme ? "moon-image" : "sun-image"} />
+                    onClick={() => toggleFunc()}>
+                    <img className="mx-auto" src={globalDarkTheme ? MoonIcon : SunIcon} 
+                    alt={globalDarkTheme ? "moon-image" : "sun-image"} />
                 </button>
             </div>
         </div>
