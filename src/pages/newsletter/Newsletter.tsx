8c3a7143bbb719c3a53e6539149884baa87474be
valts08 <valts.pilans@gmail.com>
2025-04-21 23:29:21 +0300

    added form validation to the input form in the Newsletter page

--- src/pages/newsletter/Newsletter.tsx
+++ src/pages/newsletter/Newsletter.tsx
@@ -1,9 +1,24 @@
 import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import errorIcon from "../../assets/images/icon-error.svg"
+import successIcon from "../../assets/images/icon-success.svg"
 import useThemeContext from "../../hooks/useThemeContext";
+
+interface Inputs {
+    email: string
+}
 
 const Newsletter = () => {
     const globalDarkTheme = useThemeContext()
-    const [email, setEmail] = useState("")
+    const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors, isSubmitSuccessful }
+    } = useForm<Inputs>()
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
     return (
         <section className="min-h-[750px] pt-10 pb-3">
@@ -19,9 +34,29 @@ const Newsletter = () => {
                 <span className={`${globalDarkTheme ? "text-white" : ""} text-lg font-semibold block text-justify w-98/100 py-5`}>I’d love to have you along for the ride and also hear about your own journey!</span>
                 <div className="flex flex-col">
                     <span className={`${globalDarkTheme ? "text-white" : ""}`}>Email Address</span>
-                    <input className={`${globalDarkTheme ? "bg-neutral-800 border-neutral-600 text-neutral-300" : "border-gray-300 bg-white"}  border rounded-lg mt-1 mb-3 py-2 pl-2 w-auto`} type="text" value={email} placeholder="email@example.com"
-                        onChange={(e) => setEmail(e.target.value)}/>
-                    <button type="submit" className="max-w-[150px] rounded-lg font-medium bg-blue-300 py-3 px-5 hover:cursor-pointer">Stay updated</button>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input className={`${globalDarkTheme ? "bg-neutral-800 border-neutral-600 text-neutral-300" : "border-gray-300 bg-white"} ${isSubmitSuccessful ? "border-green-700" : ""} ${errors.email?.message ? "border-red-700" : ""} border rounded-lg mt-1 py-2 pl-2 w-100/100`} type="text" placeholder="email@example.com"
+                            {...register("email",
+                            {
+                                required: "Email is required",
+                                pattern: {
+                                    value: emailRegEx,
+                                    message: "Please enter a valid email address."
+                            } })}/>
+                            {isSubmitSuccessful && 
+                                <div className="pt-2">
+                                    <img src={successIcon} alt="success-icon" className="inline" />
+                                    <span className="text-green-700 pl-2">You're subscribed! Check your email for updates.</span>
+                                </div>
+                            }
+                            {errors.email?.message && 
+                                <div className="pt-2">
+                                    <img src={errorIcon} alt="error-icon" className="inline" />
+                                    <span className="text-red-700 pl-2">{errors.email?.message}</span>
+                                </div>
+                            }
+                        <input type="submit" defaultValue={`Stay updated`} className="block max-w-[150px] rounded-lg font-medium bg-blue-300 mt-3 py-3 px-5 hover:cursor-pointer"/>
+                    </form>
                     <span className="mt-1 text-gray-500">Unsubscribe anytime. No spam, I promise 🙂</span>
                 </div>
             </article>
