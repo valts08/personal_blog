139ef3398c190b4584b9c07b3ff50dbf3f0084ae
valts08 <valts.pilans@gmail.com>
2025-04-22 23:14:04 +0300

    added underline to a title

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
         <section className="min-h-[750px]">
             <article className="pb-10 pt-5">
                 <div className='py-5'>
-                    <PageHeading>Hi, I'm Paulina</PageHeading>
+                    <PageHeading>Hi, I'm Paulina 👋</PageHeading>
                     <p className={`${globalDarkTheme ? "text-neutral-400" : ""}`}>
                     Welcome to my little corner of the internet! 
                     I'm thrilled to have you here. This space is where I'll be sharing my thoughts, experiences, and discoveries on everything from coding best practices and the latest web development trends, to my personal adventures in trying new recipes and exploring local hiking trails. 
@@ -28,7 +28,8 @@ const Home = () => {
             <Divider />
             <article className='py-10'>
                 <div>
-                    <h1 className={`${globalDarkTheme ? "text-white" : ""} text-2xl font-bold pb-4`}>Latest Articles</h1>
+                    <h1 className={`${globalDarkTheme ? "text-white" : ""} text-2xl font-bold pb-4 inline mr-2`}>Latest Articles</h1>
+                    <div className='inline-block w-[40px] border-b-4 border-sky-500/50'></div>
                     <ArticleList articles={mockArticles}/>
                 </div>
                 <Link to="/" className={`${globalDarkTheme ? "text-white" : ""} ${focusStyles} underline underline-offset-3 decoration-2 decoration-sky-500/50 hover:text-gray-600`}>View all articles</Link>
